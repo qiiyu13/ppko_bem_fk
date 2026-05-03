a3a5912437f39cc19ec748e1306d8ab600bdeada
Muhammad Fiqi Firmansyah <fiqifirmansyah5535@gmail.com>
2026-05-03 16:33:00 +0700

    feat: create ConnectivityService with auto-sync on reconnect

---
+++ lib/services/connectivity_service.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'cache_service.dart';
+
+class ConnectivityService {
+  static final ConnectivityService instance = ConnectivityService._internal();
+  ConnectivityService._internal();
+
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription<ConnectivityResult>? _subscription;
+
+  bool _isOnline = true;
+  bool get isOnline => _isOnline;
+
+  final _syncStatusController = StreamController<SyncStatus>.broadcast();
+  Stream<SyncStatus> get syncStatusStream => _syncStatusController.stream;
+
+  int _pendingSyncCount = 0;
+  int get pendingSyncCount => _pendingSyncCount;
+
+  Future<void> initialize() async {
+    final result = await _connectivity.checkConnectivity();
+    _isOnline = result != ConnectivityResult.none;
+    await _updatePendingCount();
+
+    _subscription = _connectivity.onConnectivityChanged.listen((result) async {
+      final wasOnline = _isOnline;
+      _isOnline = result != ConnectivityResult.none;
+
+      if (!wasOnline && _isOnline) {
+        await _syncOnReconnect();
+      }
+    });
+  }
+
+  Future<void> _syncOnReconnect() async {
+    _syncStatusController.add(SyncStatus.syncing);
+    try {
+      await CacheService.processSyncQueue();
+      await _updatePendingCount();
+      _syncStatusController.add(SyncStatus.completed);
+    } catch (e) {
+      _syncStatusController.add(SyncStatus.failed);
+    }
+  }
+
+  Future<void> _updatePendingCount() async {
+    _pendingSyncCount = await CacheService.getPendingSyncCount();
+  }
+
+  Future<void> triggerManualSync() async {
+    if (_isOnline) {
+      await _syncOnReconnect();
+    }
+  }
+
+  void dispose() {
+    _subscription?.cancel();
+    _syncStatusController.close();
+  }
+}
+
+enum SyncStatus { idle, syncing, completed, failed }
